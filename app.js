d9ef4b6776558ffcd9a1f79e877089000ccfd40a
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-06-01 04:08:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 function updateText() {
-    var textInput = document.getElementById('myTextarea');
-    var editor = document.getElementById('textPost');
+    let textInput = document.getElementById('myTextarea');
+    let editor = document.getElementById('textPost');
     editor.innerHTML = textInput.value;
 }
+
+
+//__modal open close__//
+
 
 document.getElementById('modalBox').addEventListener('click', function () {
     document.querySelector('.modal').style.display = 'flex';
@@ -11,3 +15,34 @@ document.getElementById('modalBox').addEventListener('click', function () {
 document.getElementById("closeModal").addEventListener('click', function () {
     document.querySelector('.modal').style.display = 'none';
 })
+
+
+
+
+//__changing Background Image__//
+
+
+function changeBackground(imageUrl) {
+    let resultDiv = document.getElementById('textPost');
+    resultDiv.style.backgroundImage = "url('" + imageUrl + "')";
+}
+
+let dropdown = document.querySelector('.dropdown')
+
+dropdown.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target.classList.contains('images-change')) {
+        let imageUrl = target.querySelector('.post-img').getAttribute('src');
+        changeBackground(imageUrl)
+    }
+
+})
+
+
+//__changing background color__//
+
+function changeBackgroundColor() {
+    let bgcolor = document.getElementById('color-input');
+    let resultDiv = document.getElementById('textPost');
+    resultDiv.style.backgroundColor = bgcolor.value;
+}
